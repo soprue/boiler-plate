6b82711c78244199fc9f6174e02bfeb1dd7facc1
hee <soprue@naver.com>
2022-08-29 19:09:54 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hee:bnhg4521@boilerplate.ziuj9lj.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected...'))
+import User from "./models/User.js";
+
+const app = express();
+const port = 3000;
+
+
+// application/x-www-form-urlencoded 데이터를 분석해서 가지고 올 수 있게 해 줌
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json 데이터를 분석해서 가지고 올 수 있게 해 줌
+app.use(bodyParser.json());
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!!')
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
